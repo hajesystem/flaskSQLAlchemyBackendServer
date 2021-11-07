692665080827f12a6c1a3b0143136ece696059b7
hajesystem <hajesystem@gmail.com>
2021-11-08 04:03:58 +0900

    create table 수정

--- model/__init__.py
+++ model/__init__.py
@@ -1,11 +1,12 @@
+import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import MetaData
 
 # DB설정 가져오기(가상환경에서 sys안됨)
 # import sys
 # sys.path.append('/home/hanjun/study/pythonSqlAlchemyFlask')
-import config
 
 # 연결설정 create_engine()
 engine = create_engine(config.mysql_conn, echo=True,
@@ -17,10 +18,10 @@ db_session = scoped_session(sessionmaker(
 
 # 매칭선언 declarative_base() : 테이블에 Mapping될 클래스를 정의하는 작업에 필요. Mapping에 필요한 클래스를 생성
 Base = declarative_base()
-# query_property() : select 할 속성을 Base에 미리 담아둔다
-Base.query = db_session.query_property()
+
+metadata = MetaData()
 
 
 def init_database():
-    # Base 생성
-    Base.metadata.create_all(bind=engine)
+    # 테이블 생성
+    metadata.create_all(bind=engine)
